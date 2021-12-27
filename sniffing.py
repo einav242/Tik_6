5e538ba0d16c9ccdd2386e81c654df22c7668eac
einav242 <93386470+einav242@users.noreply.github.com>
2021-12-28 00:39:33 +0200

    Update sniffing.py

--- sniffing.py
+++ sniffing.py
@@ -4,8 +4,9 @@ from scapy.all import *
 def print_pkt(pkt):
 	pkt.show()
 
-pkt = sniff(iface='enp0s3',filter='icmp', prn=print_pkt)
-"hi bar"
+#pkt = sniff(iface=['lo','enp0s3'],filter='icmp', prn=print_pkt) #ping 8.8.8.8
+#pkt = sniff(iface=['lo','enp0s3'],filter='tcp and src host 10.0.2.15 and dst port 23', prn=print_pkt) #telnet 8.8.8.8
+pkt = sniff(iface=['lo','enp0s3'],filter='dst net 128.230.0.0/16', prn=print_pkt) #ping 128.230.1.2
 
 
 
